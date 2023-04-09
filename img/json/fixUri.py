8f034d32fda9d9f16bc0576bb9bcf2973a6a905a
Alan Jiang <adj2424@gmail.com>
2023-04-08 23:57:34 -0400

    real nft fr

--- img/json/fixUri.py
+++
@@ -1,9 +0,0 @@
-import json
-# appended all 100 image uris' to a new base uri
-cid = 'bafybeiez5abohaxwpnzgdfub5keayojbfpx7wwr7nsaqef7aqe6pilf63y'
-for i in range(1, 101): 
-    f = open(f'{i}.json')
-    data = json.load(f)
-    data['image'] = data['image'].replace('NewUriToReplace', cid)
-    with open(f'{i}.json', 'w', encoding='utf-8') as f:
-      json.dump(data, f, ensure_ascii=False, indent=4)
